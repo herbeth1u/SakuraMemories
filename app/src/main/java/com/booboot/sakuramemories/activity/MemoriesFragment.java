9a4b296db1ea8845caea91339c33cacfa8d0d148
Thomas HERBETH <thomas.herbeth@gmail.com>
2017-04-19 20:11:16 +0200

    [+] Grass background.

--- app/src/main/java/com/booboot/sakuramemories/activity/MemoriesFragment.java
+++ app/src/main/java/com/booboot/sakuramemories/activity/MemoriesFragment.java
@@ -6,14 +6,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.booboot.sakuramemories.R;
+import com.squareup.picasso.Picasso;
 
 public class MemoriesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.memories_fragment, container, false);
 
+        ImageView background = (ImageView) rootView.findViewById(R.id.background);
+        Picasso.with(getActivity()).load(R.drawable.grass).centerCrop().fit().into(background);
         Log.e("D", "MEMORIES FRAGMENT");
         return rootView;
     }
