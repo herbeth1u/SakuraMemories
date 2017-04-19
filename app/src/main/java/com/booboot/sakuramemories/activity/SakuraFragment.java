5e0cb8aa28da87490ccff5e7eebc0ad9f45db1ba
Thomas HERBETH <thomas.herbeth@gmail.com>
2017-04-19 18:37:12 +0200

    Revert "[+] Fetch the weather with the current GPS location on OpenWeatherMap (added GPS permission checks, Retrofit and Jackson)."
    This reverts commit e1b418b0b6cb35f5b9d64c08997dcc713030c4f2.

--- app/src/main/java/com/booboot/sakuramemories/activity/SakuraFragment.java
+++ app/src/main/java/com/booboot/sakuramemories/activity/SakuraFragment.java
@@ -5,49 +5,58 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 
 import com.booboot.sakuramemories.R;
-import com.booboot.sakuramemories.util.App;
-import com.booboot.sakuramemories.util.Weather;
+import com.plattysoft.leonids.ParticleSystem;
 
 public class SakuraFragment extends Fragment {
     public static final String WEATHER_START_INTENT = "WEATHER_START_INTENT";
     public static final String WEATHER_STOP_INTENT = "WEATHER_STOP_INTENT";
-    private Weather weather;
+    private View cloud;
+    private ParticleSystem particleSystem;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.sakura_fragment, container, false);
 
+        cloud = getActivity().findViewById(R.id.cloud);
         LocalBroadcastManager.getInstance(getActivity()).registerReceiver(messageReceiver, new IntentFilter(WEATHER_START_INTENT));
         LocalBroadcastManager.getInstance(getActivity()).registerReceiver(messageReceiver, new IntentFilter(WEATHER_STOP_INTENT));
-        weather = new Weather(getActivity(), this, getActivity().findViewById(R.id.cloud), rootView.findViewById(R.id.weatherOverlay));
+        startWeather();
 
         return rootView;
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case App.ACCESS_FINE_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted
-                    weather.getWeather();
-                }
-                break;
+    private void startWeather() {
+        particleSystem = new ParticleSystem(getActivity(), 30 * 8000 / 1000, R.drawable.rain, 8000)
+                .setAcceleration(0.00013f, 90 - (-6))
+                .setInitialRotationRange(-6, 6)
+                .setSpeedByComponentsRange(0f, 0f, 0.05f, 0.1f)
+                //.setSpeedRange(0.2f, 0.5f)
+                .setFadeOut(7000, new AccelerateInterpolator());
+
+        cloud.post(new Runnable() {
+            @Override
+            public void run() {
+                particleSystem.emitWithGravity(cloud, Gravity.BOTTOM, 30);
             }
-        }
+        });
+    }
+
+    private void stopWeather() {
+        if (particleSystem != null) particleSystem.cancel();
     }
 
     @Override
     public void onDestroyView() {
-        weather.stopWeather();
+        stopWeather();
         LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(messageReceiver);
         super.onDestroyView();
     }
@@ -57,11 +66,11 @@ public class SakuraFragment extends Fragment {
         public void onReceive(Context context, Intent intent) {
             switch (intent.getAction()) {
                 case WEATHER_START_INTENT:
-                    weather.startWeather();
+                    startWeather();
                     break;
 
                 case WEATHER_STOP_INTENT:
-                    weather.stopWeather();
+                    stopWeather();
                     break;
             }
         }
